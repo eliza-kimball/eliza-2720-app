38a906308ce6c9a2b71293a32f254654728f72cc
eliza-kimball <156148008+eliza-kimball@users.noreply.github.com>
2025-01-09 15:43:24 -0700

    cleaned up files

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import {resolve} from 'path'
+import { defineConfig } from 'vite'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
+export default defineConfig({
+    root,
+    build: {
+        outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            main: resolve(root, 'index.html'),
+        }
+    },
+})
